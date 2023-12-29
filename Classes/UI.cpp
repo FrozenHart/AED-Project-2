e29bed7afaa61526adf0157bdcc1a1b0481ae8f7
Raquel Fernandes <raquelgfernandes88@gmail.com>
2023-12-29 00:03:15 -0100

    menu

--- Classes/UI.cpp
+++ Classes/UI.cpp
@@ -1,1 +1,30 @@
 #include "UI.h"
+
+void UI::run(FileReader x) {
+    while(true) {
+
+    }
+}
+
+string UI::print_menu() {
+    string ss;
+    ss += "-----------------------------------------------------------------\n";
+    ss += "|        Menu                                                   |\n";
+    ss += "|        Number of Airports                                     |\n";
+    ss += "|        Number of Flights                                      |\n";
+    ss += "|        Number of Flights from Airport                         |\n";
+    ss += "|        Number of Airlines from Airport                        |\n";
+    ss += "|        Number of Flights from City                            |\n";
+    ss += "|        Number of Flights from each Airport                    |\n";
+    ss += "|        Number of Flights from City to Different Countries     |\n";
+    ss += "|        Number of Flights from Airport to Different Countries  |\n";
+    ss += "|        Number of Destinations from Airport                    |\n";
+    ss += "|        Number of Destinations from City                       |\n";
+    ss += "|        Number of Destinations from Country                    |\n";
+    ss += "|        Number of Reachable Destinations from Airport          |\n";
+    ss += "|        Number of Reachable Destinations from City             |\n";
+    ss += "|        Number of Reachable Destinations from Country          |\n";
+    ss += "-----------------------------------------------------------------\n";
+
+    return std::string();
+}
--- Classes/UI.h
+++ Classes/UI.h
@@ -2,8 +2,13 @@
 #ifndef AED_PROJECT_2_UI_H
 #define AED_PROJECT_2_UI_H
 
+#include <iostream>
+#include "FileReader.h"
 
 class UI {
+public:
+    void run(FileReader x);
+    string print_menu();
 
 };
 
